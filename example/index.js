ed20d646bfbd653e7baabd1c3dc4dbc907911268
Googler <knowbeeinc@gmail.com>
2019-12-01 09:23:12 +0200

    get date and full date

--- example/index.js
+++ example/index.js
@@ -1,4 +1,16 @@
-const { getDay, getMonth, getMonthNumber } = require("../lib");
+const {
+  getDay,
+  getMonth,
+  getMonthNumber,
+  getYear,
+  getWeekdayNumber,
+  getFullDate,
+  getDate
+} = require("../lib");
 console.log(`\ngetDay() =>`, getDay());
 console.log(`\ngetMonth() =>`, getMonth());
 console.log(`\ngetMonthNumber() =>`, getMonthNumber());
+console.log(`\ngetYear() =>`, getYear());
+console.log(`\ngetWeekdayNumber() =>`, getWeekdayNumber());
+console.log(`\ngetDate() =>`, getDate());
+console.log(`\ngetFullDate() =>`, getFullDate());
