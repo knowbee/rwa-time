4b84678ac094d44b71bf81338b22e2ce59e86fb9
Googler <knowbeeinc@gmail.com>
2019-12-01 09:34:01 +0200

    get current month, current hour

--- example/index.js
+++ example/index.js
@@ -7,7 +7,10 @@ const {
   getFullDate,
   getDate,
   getFullCityDate,
-  getFullWeekdayDate
+  getFullWeekdayDate,
+  getMonthYear,
+  getMonthNumberYear,
+  getHour
 } = require("../lib");
 console.log(`\ngetDay() =>`, getDay());
 console.log(`\ngetMonth() =>`, getMonth());
@@ -18,3 +21,6 @@ console.log(`\ngetDate() =>`, getDate());
 console.log(`\ngetFullDate() =>`, getFullDate());
 console.log(`\ngetFullCityDate() =>`, getFullCityDate("Kigali"));
 console.log(`\ngetFullWeekdayDate() =>`, getFullWeekdayDate());
+console.log(`\ngetMonthYear() =>`, getMonthYear());
+console.log(`\ngetMonthNumberYear() =>`, getMonthNumberYear());
+console.log(`\ngetHour() =>`, getHour());
